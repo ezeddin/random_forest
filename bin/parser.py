f17f6d71d073e24fdc09f97fc0e46c37e2c77cbe
Ezeddin Al Hakim <ezeddin@n131-p69.kthopen.kth.se>
2017-01-05 16:32:50 +0100

    implemented dataset parser

--- bin/parser.py
+++ bin/parser.py
@@ -10,11 +10,8 @@ IONOSPHERE_PATH = ('../data/6.Ionosphere/ionosphere.data.txt', None)
 DIABETES_PATH = ('../data/7.Diabetes/pima-indians-diabetes.data', np.float)
 SONER_PATH = ('../data/8.Sonar/sonar.all-data.txt', None)
 BREAST_CANCER_PATH = ('../data/9.Breast Cancer/breast-cancer-wisconsin.data.txt', np.float)
-
+path_list = [ECOLI_PATH, GLASS_PATH, LIVER_PATH, LETTERS_PATH, SAT_IMAGES_PATH, WAVEFORM_PATH, IONOSPHERE_PATH, DIABETES_PATH, SONER_PATH, BREAST_CANCER_PATH]
 
 def parse_dataset(path_list):
     return [np.genfromtxt(data_path, delimiter=',', dtype=data_type) for data_path, data_type in path_list]
 
-path_list = [ECOLI_PATH, GLASS_PATH, LIVER_PATH, LETTERS_PATH, SAT_IMAGES_PATH, WAVEFORM_PATH, IONOSPHERE_PATH, DIABETES_PATH, SONER_PATH, BREAST_CANCER_PATH]
-datasets = parse_dataset(path_list)
-print(datasets[5][0])
