d0f732ed1a54c7c603953847a970edd5974c6259
Ezeddin Al Hakim <ezeddin@n131-p69.kthopen.kth.se>
2017-01-05 18:37:47 +0100

    implemented entropy and information gain

--- bin/forest.py
+++ bin/forest.py
@@ -5,27 +5,38 @@ class Forest(object):
         self.n_tree = n_tree
         self.n_features = n_features
  
-    def entropy(self, d):
+    def entropy(self, labels):
+            unique, counts = np.unique(labels, return_counts=True)
             E = 0.0
-            for r in d.keys():
-                if d['count'] > 0 and d[r] > 0:
-                    proba = float(d[r]) / d['count']
+            for i in range(len(unique)):
+                if counts[i] > 0:
+                    proba = counts[i] / np.sum(counts)
                     E -= proba * np.log(proba)
             return E
+    
+    def information_gain(self, dataset, labels, feature):
+        unique, counts = np.unique(dataset[:feature], return_counts=True)
+        gain = self.entropy(labels)
+        for i, v in enumerate(unique):
+            if counts[i] > 0:
+                w = counts[i] / np.sum(counts)
+                gain -= w * self.entropy([labels[i] for row in dataset if row[feature] == v])
+        return gain
 
-    def build_tree(self, dataset):
+    def build_tree(self, dataset, labels):
         for l in self.n_tree:
             d_tilde = self.dataset_split(dataset)
             d_tilde_f = self.random_features(d_tilde)
             for f in range(self.n_features):
                 pass
 
-
     def predcit(self):
         pass
-        
-datasets = get_datasets()
-glass = datasets[1]
-unique, counts = np.unique(glass, return_counts=True)
 
-print(unique)
+
+datasets = get_datasets()
+X = datasets[1][:,1:-1]
+Y = datasets[1][:,-1]
+
+forest = Forest(10,10)
+print(forest.information_gain(X,Y,3))
