dcaa4c06683cedca07415d3e9d267cf39107408e
HenFla <flamme@kth.se>
2017-01-07 16:38:25 +0100

    Add files via upload

--- bin/parser.py
+++ bin/parser.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-ECOLI_PATH = ('../data/0.Ecoli/ecoli.data.txt', None)
-GLASS_PATH = ('../data/1.Glass/glass.data', np.float)
-LIVER_PATH = ('../data/2.Liver/bupa.data', np.float)
-LETTERS_PATH = ('../data/3.Letters/letter-recognition.data.txt', None)
+ECOLI_PATH = ('../data/0.Ecoli/Xy.txt', None)
+GLASS_PATH = ('../data/1.Glass/Xy.txt', np.float)
+LIVER_PATH = ('../data/2.Liver/Xy.txt', np.float)
+LETTERS_PATH = ('../data/3.Letters/Xy.txt', None)
 SAT_IMAGES_PATH = ('../data/4.Sat Images/sat.all', np.float)
 WAVEFORM_PATH = ('../data/5.Waveform/waveform-+noise.data', np.float)
 IONOSPHERE_PATH = ('../data/6.Ionosphere/ionosphere.data.txt', None)
-DIABETES_PATH = ('../data/7.Diabetes/pima-indians-diabetes.data', np.float)
+DIABETES_PATH = ('../data/7.Diabetes/Xy.txt', np.float)
 SONER_PATH = ('../data/8.Sonar/sonar.all-data.txt', None)
-BREAST_CANCER_PATH = ('../data/9.Breast Cancer/breast-cancer-wisconsin.data.txt', np.float)
+BREAST_CANCER_PATH = ('../data/9.Breast Cancer/Xy.txt', np.float)
 path_list = [ECOLI_PATH, GLASS_PATH, LIVER_PATH, LETTERS_PATH, SAT_IMAGES_PATH, WAVEFORM_PATH, IONOSPHERE_PATH, DIABETES_PATH, SONER_PATH, BREAST_CANCER_PATH]
 
 def parse_dataset(path_list):
@@ -17,3 +17,5 @@ def parse_dataset(path_list):
 
 def get_datasets():
     return parse_dataset(path_list)
+
+
