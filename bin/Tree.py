5c0536fdc21ce08d338f1733d5a5941086175529
HenFla <flamme@kth.se>
2017-01-11 18:20:39 +0100

    Add files via upload

---
+++ bin/Tree.py
@@ -0,0 +1,91 @@
+
+class Tree(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.split_index = None
+        self.split_value = None
+        self.label = None
+
+    # Split and create sub trees
+    def split(self, dataset, features, max_depth):
+        if max_depth >= 1:
+
+            # Find a good split
+            split_index, split_value, groups = self.get_split(dataset, features)
+            left = groups[0]
+            right = groups[1]
+
+            # If no senseful split was found
+            if left == None or right == None:
+                self.set_label(dataset)
+            else:
+                # Remove 'b_index' from 'features'
+                features = list(set(features).remove(b_index))
+
+                # Set sub trees
+                self.split_index = split_index
+                self.split_value = split_value
+                self.left = Tree()
+                self.right = Tree()
+
+                # Are groups large enough to be splitted again?
+                if left.shape[0] >= min_n:
+                    self.left.split( left, features, max_depth-1)
+                else:
+                    self.left.set_label( left )
+
+                if right.shape[0] >= min_n:
+                    self.right.split( right, features, max_depth-1)
+                else:
+                    self.right.set_label( right )
+        else:
+            self.set_label(dataset)
+
+    def set_label(self, dataset):
+        y = dataset(:,-1)
+        self.label = max( set(y), key=y.count )
+
+    def get_split(self, dataset, features):
+        class_values = list(set(row[-1] for row in dataset))
+        b_index, b_value, b_score, b_groups = None, None, 999, None
+
+        for index in features:
+            for row in dataset:
+                groups = test_split(index, row[index], dataset)
+                gini = gini_index(groups, class_values)
+                if gini < b_score:
+                    b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+        return b_index, b_value, b_groups
+
+    # Split a dataset based on the given feature index and its corresponding value
+    def test_split(self, index, value, dataset):
+        left, right = list(), list()
+        for row in dataset:
+            if row[index] < value:
+                left.append(row)
+            else:
+                right.append(row)
+        return left, right
+
+    def predict(self, dataset):
+        for row in dataset:
+            if row[self.split_index] < self.split_value:
+                if self.left == None:
+                    return self.label
+                else:
+                    self.left.predict(dataset)
+            else:
+                if self.right == None:
+                    return self.label
+                else:
+                    self.right.predict(dataset)
+
+    # Indicates how good a split is (0.0 is a pure split)
+    def gini_index(self, groups, class_values):
+        gini = 0.0
+        for class_value in class_values:
+            for group in groups:
+                if len(groups) > 0:
+                    proportion = [row[-1] for row in group].count(class_value) / float(len(groups))
+                    gini += proportion * (1.0-proportion)
--- bin/forest.py
+++ bin/forest.py
@@ -1,10 +1,14 @@
 from parser import *
+import random
+from Tree import *
 
 class Forest(object):
-    def __init__(self, n_tree, n_features):
-        self.n_tree = n_tree
-        self.n_features = n_features
- 
+    def __init__(self, n_trees, n_features, max_depth=1):
+        self.n_trees = n_trees
+        self.n_features = n_features # n_features = max_depth ????
+        self.trees = list()
+        self.max_depth = max_depth # n_features = max_depth ????
+
     def entropy(self, labels):
             unique, counts = np.unique(labels, return_counts=True)
             E = 0.0
@@ -13,7 +17,7 @@ class Forest(object):
                     proba = counts[i] / np.sum(counts)
                     E -= proba * np.log(proba)
             return E
-    
+
     def information_gain(self, dataset, labels, feature):
         unique, counts = np.unique(dataset[:feature], return_counts=True)
         gain = self.entropy(labels)
@@ -24,14 +28,42 @@ class Forest(object):
         return gain
 
     def build_tree(self, dataset, labels):
-        for l in self.n_tree:
+        for l in self.n_trees:
             d_tilde = self.dataset_split(dataset)
             d_tilde_f = self.random_features(d_tilde)
             for f in range(self.n_features):
                 pass
 
-    def predcit(self):
-        pass
+    # X : complete dataset
+    # y : labels
+    # n_test : desired training set size
+    def build_tree_new(self, dataset, n_test):
+        self.trees = list() # delete old trees
+        for t in self.n_trees:
+            Xy = self.dataset_split(dataset, n_test)
+            features = self.random_features(Xy)
+            new_tree = Tree()
+            new_tree.split( Xy, features, self.max_depth )
+            self.trees.append( new_tree )
+
+            # DO CROOS VALIDATION => data does not have to be stored in each tree
+            #new_tree.predict( test_set )
+
+    # sample features WITHOUT replacement
+    def random_features(self, Xy):
+        return list(random.sample( range(Xy.shape[1]-1) , self.n_features ))
+
+    # sample n times from data WITH replacement
+    def dataset_split(self, dataset, n):
+        idx = list(random.choice(range(dataset.data[0]),n))
+        Xy = [ dataset[i,:] for i in idx]
+        return Xy
+
+    def predict(self):
+        for tree in self.trees:
+            tree.predict()
+            # ...
+
 
 
 datasets = get_datasets()
