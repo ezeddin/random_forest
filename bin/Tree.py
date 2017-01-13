98dc0b5b8d17bb434d2247dc8a48057f8409225c
Ezeddin Al Hakim <ezeddin@n155-p122.kthopen.kth.se>
2017-01-13 09:52:58 +0100

    first verison of random forest

--- bin/Tree.py
+++ bin/Tree.py
@@ -1,91 +1,82 @@
-
+import numpy as np
+import pdb
 class Tree(object):
     def __init__(self):
         self.left = None
         self.right = None
-        self.split_index = None
+        self.split_feature = None
         self.split_value = None
-        self.label = None
+        self.node = None
 
     # Split and create sub trees
-    def split(self, dataset, features, max_depth):
+    def split(self, dataset, labels, features, max_depth, min_size=1):
         if max_depth >= 1:
-
             # Find a good split
-            split_index, split_value, groups = self.get_split(dataset, features)
-            left = groups[0]
-            right = groups[1]
+            gini_value, split_feature, split_value, branches = self.get_split(dataset, labels, features)
 
             # If no senseful split was found
-            if left == None or right == None:
-                self.set_label(dataset)
+            if gini_value == :
+                self.last_node(labels)
             else:
-                # Remove 'b_index' from 'features'
-                features = list(set(features).remove(b_index))
 
+                left = branches[0]
+                right = branches[1]
+
+                features.remove(split_feature)
                 # Set sub trees
-                self.split_index = split_index
+                self.split_feature = split_feature
                 self.split_value = split_value
                 self.left = Tree()
                 self.right = Tree()
 
-                # Are groups large enough to be splitted again?
-                if left.shape[0] >= min_n:
-                    self.left.split( left, features, max_depth-1)
+                # Are branches large enough to be splitted again?
+                if sum(left) >= min_size:
+                    self.left.split(dataset[left], labels[left], features, max_depth-1)
                 else:
-                    self.left.set_label( left )
+                    self.left.last_node(labels[left])
 
-                if right.shape[0] >= min_n:
-                    self.right.split( right, features, max_depth-1)
+                if sum(right) >= min_size:
+                    self.right.split(dataset[right], labels[right], features, max_depth-1)
                 else:
-                    self.right.set_label( right )
+                    self.right.last_node(labels[right])
         else:
-            self.set_label(dataset)
+            self.last_node(labels)
 
-    def set_label(self, dataset):
-        y = dataset(:,-1)
-        self.label = max( set(y), key=y.count )
+    def last_node(self, labels):
+        pdb.set_trace()
+        labels = list(labels)
+        self.node = max(set(labels), key=labels.count)
 
-    def get_split(self, dataset, features):
-        class_values = list(set(row[-1] for row in dataset))
-        b_index, b_value, b_score, b_groups = None, None, 999, None
-
-        for index in features:
-            for row in dataset:
-                groups = test_split(index, row[index], dataset)
-                gini = gini_index(groups, class_values)
-                if gini < b_score:
-                    b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-        return b_index, b_value, b_groups
-
-    # Split a dataset based on the given feature index and its corresponding value
-    def test_split(self, index, value, dataset):
-        left, right = list(), list()
+    def get_split(self, dataset, labels, features):
+        gini_list = list()
         for row in dataset:
-            if row[index] < value:
-                left.append(row)
-            else:
-                right.append(row)
-        return left, right
+            for feature in features:
+                value = row[feature]        
+                branches = (dataset[:,feature]<value, dataset[:,feature]>=value) 
+                gini_value = self.gini(branches, labels)
+                gini_list.append((gini_value,feature,value,branches))
+        if len(gini_list) != 0:
+            return max(gini_list, key=lambda x: x[0])
+        else:
+            return None, None, None, None
 
-    def predict(self, dataset):
-        for row in dataset:
-            if row[self.split_index] < self.split_value:
-                if self.left == None:
-                    return self.label
-                else:
-                    self.left.predict(dataset)
+    def predict(self, x):
+        if x[self.split_feature] < self.split_value:
+            if self.left == None:
+                return self.label
             else:
-                if self.right == None:
-                    return self.label
-                else:
-                    self.right.predict(dataset)
+                self.left.predict(x)
+        else:
+            if self.right == None:
+                return self.label
+            else:
+                self.right.predict(x)
 
-    # Indicates how good a split is (0.0 is a pure split)
-    def gini_index(self, groups, class_values):
+    # Gini impurity
+    def gini(self, branches, labels):
         gini = 0.0
-        for class_value in class_values:
-            for group in groups:
-                if len(groups) > 0:
-                    proportion = [row[-1] for row in group].count(class_value) / float(len(groups))
-                    gini += proportion * (1.0-proportion)
+        for branch in branches:
+            unique, counts = np.unique(labels[branch], return_counts=True)
+            proportion = counts / float(sum(counts)) 
+            gini += sum(proportion*(1-proportion))
+        return gini
--- bin/forest.py
+++ bin/forest.py
@@ -1,85 +1,40 @@
 from parser import *
 import random
-# from Tree import *
+from Tree import *
 
 class Forest(object):
-    def __init__(self, n_trees, n_features, subset_ratio=0.5, max_depth=1):
+    def __init__(self, n_trees, n_features, subset_ratio=0.5, max_depth=2):
         self.n_trees = n_trees
         self.n_features = n_features # n_features = max_depth ????
         self.subset_ratio = subset_ratio
-        self.trees = list()
         self.max_depth = max_depth # n_features = max_depth ????
-
-    def entropy(self, labels):
-            unique, counts = np.unique(labels, return_counts=True)
-            E = 0.0
-            for i in range(len(unique)):
-                if counts[i] > 0:
-                    proba = counts[i] / np.sum(counts)
-                    E -= proba * np.log(proba)
-            return E
-
-    def information_gain(self, dataset, labels, feature):
-        unique, counts = np.unique(dataset[:feature], return_counts=True)
-        gain = self.entropy(labels)
-        for i, v in enumerate(unique):
-            if counts[i] > 0:
-                w = counts[i] / np.sum(counts)
-                gain -= w * self.entropy([labels[j] for j in range(len(dataset)) if dataset[j,feature] == v])
-        return gain
     
     def subset_dataset(self, dataset):
         n_dataset_samples = dataset.shape[0]
-        n_subset_samples =  n_dataset_samples * self.subset_ratio
+        n_subset_samples =  int(n_dataset_samples * self.subset_ratio)
         return np.random.randint(n_dataset_samples, size=n_subset_samples)
 
-    def build_tree(self, subset_idx, features, dataset, labels):
-        max_gain = [(f, self.information_gain(dataset[subset_idx,:], labels[subset_idx], f)) for f in features]
-        print(max(max_gain), max_gain)
-
-    def evaluate(self, dataset, labels):
-        trees = list()
-        for _ in range(self.n_trees):
-            subset_idx = self.subset_dataset(dataset)
-            features = np.random.choice(dataset.shape[1], self.n_features, replace=False)
-            trees.append(self.build_tree(subset_idx, features, dataset, labels))
-
-    # X : complete dataset
-    # y : labels
-    # n_test : desired training set size
     def build_tree_new(self, dataset, n_test):
         self.trees = list() # delete old trees
         for t in self.n_trees:
             Xy = self.dataset_split(dataset, n_test)
             features = self.random_features(Xy)
+
+    def evaluate(self, dataset, labels):
+        trees = list()
+        for _ in range(self.n_trees):
+            subset_idx = self.subset_dataset(dataset)
+            features = set(np.random.choice(dataset.shape[1], self.n_features, replace=False))
             new_tree = Tree()
-            new_tree.split( Xy, features, self.max_depth )
-            self.trees.append( new_tree )
-
-            # DO CROOS VALIDATION => data does not have to be stored in each tree
-            #new_tree.predict( test_set )
-
-    # sample features WITHOUT replacement
-    def random_features(self, Xy):
-        return list(random.sample( range(Xy.shape[1]-1) , self.n_features ))
-
-    # sample n times from data WITH replacement
-    def dataset_split(self, dataset, n):
-        idx = list(random.choice(range(dataset.data[0]),n))
-        Xy = [ dataset[i,:] for i in idx]
-        return Xy
-
-    def predict(self):
-        for tree in self.trees:
-            tree.predict()
-            # ...
-
-
-
+            new_tree.split( dataset[subset_idx], labels[subset_idx], features, self.max_depth )
+            trees.append(new_tree)
+        return trees
+        
 datasets = get_datasets()
 X = datasets[1][:,0:-1]
 Y = datasets[1][:,-1]
 
-forest = Forest(n_trees=1,n_features=3)
-forest.evaluate(X,Y)
+forest = Forest(n_trees=1,n_features=3, max_depth=10)
+trees = forest.evaluate(X,Y)
+print(trees[0].predict(X[0]), Y[0])
 # print(forest.information_gain(X,Y,2))
